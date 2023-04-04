eea5f502819f8ad5d17973d30d5fec2596c9961c
Chris <hakizachrist1@gmail.com>
2023-04-04 19:07:11 +0200

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,4 +1,4 @@
-INSERT INTO animals (animal_name, date_of_birth, escape_attempts,
+INSERT INTO animals (name, date_of_birth, escape_attempts,
 neutered, weight_kg) 
 VALUES 
 ('Agumon', '2020-02-03', 0, FALSE,10.23),
